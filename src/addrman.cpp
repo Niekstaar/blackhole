d26d98812f64d09480b43bb1b43beaf17abc1012
Blackwolf <none@none.net>
2014-04-05 11:47:29 +0200

    Reworked the original source

--- src/addrman.cpp
+++ src/addrman.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) 2012 Pieter Wuille
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-// TEST
 
 #include "addrman.h"
 
